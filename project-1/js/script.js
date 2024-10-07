d4125cdc0a97b7648413562ff068e72799fe8a55
Rickey Chang <changr1@gmatc.matc.edu>
2024-10-06 19:24:26 -0500

    update project-1

---
+++ project-1/js/script.js
@@ -0,0 +1,4 @@
+var price = 20;
+
+document.getElementById('price').textContent = price;
+document.getElementById("p1").textContent = 'Bettas are a very beautiful and colorful type of fish. There are many types and is very noticeable within the males.';
